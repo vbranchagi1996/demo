2a812d00fe1a474f868dd7c8fc3f85789c869ff9
vbranchagi1996 <vbranchagi@gmail.com>
2018-08-24 15:52:01 +0530

    Main File

---
+++ calculator.java
@@ -0,0 +1,20 @@
+//Import req packages
+import java.io.*;
+//class def
+class calculator
+{
+	//main
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		int diff=a-b;
+		int prod=a*b;
+		int quo=a/b;
+		System.out.println("Addition of a and b=" +sum);
+		System.out.println("Subtraction of a and b=" +diff);
+		System.out.println("Product of a and b=" +prod);
+		System.out.println("Quotient of a and b=" +quo);
+	}
+}
